fc6a171658199d1d67b013bea66b79870bd1bb73
ngngochien <ngochien.140604@gmail.com>
2025-03-01 15:09:33 +0700

    add

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css'
+import ProductIntro from './components/ProductIntro/ProductIntro'
 import Home from './pages/Home/Home'
 import Home2 from './pages/Home/Home2'
 import Home3 from './pages/Home/Home3'
 import Home4 from './pages/Home/Home4'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
 
   return (
-    <>
-      <Home2/>
-    </>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/product' element={<ProductIntro/>} />
+        <Route path='/home2' element={<Home2/>} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
